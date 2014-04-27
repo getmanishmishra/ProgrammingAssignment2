44c6a422a98c0378e5b2e99ecd3084dd9825e5f1
getmanishmishra <getmanishmishra@gmail.com>
2014-04-27 19:16:32 +0530

    Implemented the Caching the Inverse of a Matrix functionality for "Programming Assignment 2"

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
 # Here we are writing two functions which will be used to cache the inverse of a matrix
 
-
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
         i<-null
